4e3d2560981511675e1c4f41c869b4e0cf940eee
orgguild <Orgguild@gmail.com>
2025-06-04 13:06:08 +0000

    script update

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const playlist = [
   'videos/video1.mp4',
   'videos/video2.mp4',
   'videos/video3.mp4',
-  'videos/video4.mp4',
-  'videos/video5.mp4',
+  //'videos/video4.mp4',
+  //'videos/video5.mp4',
   //'videos/video6.mp4',
   //'videos/video7.mp4',
   //'videos/video8.mp4'
