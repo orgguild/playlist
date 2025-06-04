495c9fdf4dbe35c0d0f387cf1f46147f56bf29d3
orgguild <Orgguild@gmail.com>
2025-06-04 14:02:12 +0000

    new

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const videoPlayer = document.getElementById('videoPlayer');
 const playlist = [
   'videos/video1.mp4',
   'videos/video2.mp4',
+  'videos/video1.2.mp4',
   'videos/video3.mp4',
+  'videos/video1.1.mp4',
   //'videos/video4.mp4',
   //'videos/video5.mp4',
   //'videos/video6.mp4',
