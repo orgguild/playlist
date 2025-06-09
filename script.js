98429de2beecd9a82d6dc93cb36f83230224550d
orgguild <Orgguild@gmail.com>
2025-06-09 17:52:07 +0000

    video upd

--- script.js
+++ script.js
@@ -2,14 +2,8 @@ const videoPlayer = document.getElementById('videoPlayer');
 
 // List of video file paths
 const playlist = [
-  'videos/video1.mp4',
-  'videos/video2.mp4',
-  'videos/video3.mp4',
-  //'videos/video4.mp4',
-  //'videos/video5.mp4',
-  //'videos/video6.mp4',
-  //'videos/video7.mp4',
-  //'videos/video8.mp4'
+  'videos/NewTone.MP4',
+  'videos/Sojka.MP4'
 ];
 
 let currentIndex = 0;
